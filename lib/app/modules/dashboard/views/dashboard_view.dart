3a181d659fe96aeb82db8c1cc333f2314f37eae6
Azmil2 <azmil01909@gmaol.com>
2023-02-25 18:37:53 +0700

    first commit

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -52,15 +52,268 @@ Widget build(BuildContext context) {
             ],
           ),
         ),
-        body: const TabBarView(
+        body:  TabBarView(
           children: [
-            Center(child: Text('Berita Headline')),
-            Center(child: Text('Berita Teknologi')),
-            Center(child: Text('Berita Sains')),
+            headline(),
+            teknologi(),
+            sains(),
           ],
         ),
       ),
     ),
   );
 }
+
+// Membuat sebuah ListView yang mengandung satu container dengan gambar, judul, dan sumber berita
+ListView headline() {
+    return ListView(
+      shrinkWrap: true, // Mengatur agar ListView tidak memakan ruang kosong yang tidak diperlukan
+      children: [
+        Container(
+          // Membuat sebuah container untuk mengandung gambar, judul, dan sumber berita
+          padding: const EdgeInsets.only(top: 5, left: 8, right: 8, bottom: 5), // Mengatur padding di dalam container
+          height: 110, // Mengatur tinggi container
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam row secara vertikal di awal
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0), // Mengatur radius border gambar
+                child: Image.network(
+                  'https://picsum.photos/100', // Mengambil gambar dari url
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // Mengatur tata letak anak-anak dalam column secara vertikal dengan spasi yang sama di antara mereka
+                  children: [
+                    const Text('Sri Mulyani Kecam Hidup Mewah Pejabat Pajak Buntut Kasus Rubicon - CNN Indonesia'), // Menampilkan judul berita
+                    const SizedBox(
+                      height: 2,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                      children: const [
+                        Text('Author : Muhammad Azwar'), // Menampilkan nama penulis berita
+                        Text('Sumber : detik.com'), // Menampilkan sumber berita
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+         Container(
+          // Membuat sebuah container untuk mengandung gambar, judul, dan sumber berita
+          padding: const EdgeInsets.only(top: 5, left: 8, right: 8, bottom: 5), // Mengatur padding di dalam container
+          height: 110, // Mengatur tinggi container
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam row secara vertikal di awal
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0), // Mengatur radius border gambar
+                child: Image.network(
+                  'https://picsum.photos/100', // Mengambil gambar dari url
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // Mengatur tata letak anak-anak dalam column secara vertikal dengan spasi yang sama di antara mereka
+                  children: [
+                    const Text('Sri Mulyani Kecam Hidup Mewah Pejabat Pajak Buntut Kasus Rubicon - CNN Indonesia'), // Menampilkan judul berita
+                    const SizedBox(
+                      height: 2,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                      children: const [
+                        Text('Author : Muhammad Azwar'), // Menampilkan nama penulis berita
+                        Text('Sumber : detik.com'), // Menampilkan sumber berita
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  ListView teknologi() {
+    return ListView(
+      shrinkWrap: true, // Mengatur agar ListView tidak memakan ruang kosong yang tidak diperlukan
+      children: [
+        Container(
+          // Membuat sebuah container untuk mengandung gambar, judul, dan sumber berita
+          padding: const EdgeInsets.only(top: 5, left: 8, right: 8, bottom: 5), // Mengatur padding di dalam container
+          height: 110, // Mengatur tinggi container
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam row secara vertikal di awal
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0), // Mengatur radius border gambar
+                child: Image.network(
+                  'https://picsum.photos/100', // Mengambil gambar dari url
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // Mengatur tata letak anak-anak dalam column secara vertikal dengan spasi yang sama di antara mereka
+                  children: [
+                    const Text('Sri Mulyani Kecam Hidup Mewah Pejabat Pajak Buntut Kasus Rubicon - CNN Indonesia'), // Menampilkan judul berita
+                    const SizedBox(
+                      height: 2,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                      children: const [
+                        Text('Author : Muhammad Azwar'), // Menampilkan nama penulis berita
+                        Text('Sumber : detik.com'), // Menampilkan sumber berita
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+         Container(
+          // Membuat sebuah container untuk mengandung gambar, judul, dan sumber berita
+          padding: const EdgeInsets.only(top: 5, left: 8, right: 8, bottom: 5), // Mengatur padding di dalam container
+          height: 110, // Mengatur tinggi container
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam row secara vertikal di awal
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0), // Mengatur radius border gambar
+                child: Image.network(
+                  'https://picsum.photos/100', // Mengambil gambar dari url
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // Mengatur tata letak anak-anak dalam column secara vertikal dengan spasi yang sama di antara mereka
+                  children: [
+                    const Text('Sri Mulyani Kecam Hidup Mewah Pejabat Pajak Buntut Kasus Rubicon - CNN Indonesia'), // Menampilkan judul berita
+                    const SizedBox(
+                      height: 2,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                      children: const [
+                        Text('Author : Muhammad Azwar'), // Menampilkan nama penulis berita
+                        Text('Sumber : detik.com'), // Menampilkan sumber berita
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+ListView sains() {
+    return ListView(
+      shrinkWrap: true, // Mengatur agar ListView tidak memakan ruang kosong yang tidak diperlukan
+      children: [
+        Container(
+          // Membuat sebuah container untuk mengandung gambar, judul, dan sumber berita
+          padding: const EdgeInsets.only(top: 5, left: 8, right: 8, bottom: 5), // Mengatur padding di dalam container
+          height: 110, // Mengatur tinggi container
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam row secara vertikal di awal
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0), // Mengatur radius border gambar
+                child: Image.network(
+                  'https://picsum.photos/100', // Mengambil gambar dari url
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // Mengatur tata letak anak-anak dalam column secara vertikal dengan spasi yang sama di antara mereka
+                  children: [
+                    const Text('Sri Mulyani Kecam Hidup Mewah Pejabat Pajak Buntut Kasus Rubicon - CNN Indonesia'), // Menampilkan judul berita
+                    const SizedBox(
+                      height: 2,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                      children: const [
+                        Text('Author : Muhammad Azwar'), // Menampilkan nama penulis berita
+                        Text('Sumber : detik.com'), // Menampilkan sumber berita
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+         Container(
+          // Membuat sebuah container untuk mengandung gambar, judul, dan sumber berita
+          padding: const EdgeInsets.only(top: 5, left: 8, right: 8, bottom: 5), // Mengatur padding di dalam container
+          height: 110, // Mengatur tinggi container
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam row secara vertikal di awal
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0), // Mengatur radius border gambar
+                child: Image.network(
+                  'https://picsum.photos/100', // Mengambil gambar dari url
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // Mengatur tata letak anak-anak dalam column secara vertikal dengan spasi yang sama di antara mereka
+                  children: [
+                    const Text('Sri Mulyani Kecam Hidup Mewah Pejabat Pajak Buntut Kasus Rubicon - CNN Indonesia'), // Menampilkan judul berita
+                    const SizedBox(
+                      height: 2,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, // Mengatur tata letak anak-anak dalam column secara horizontal di awal
+                      children: const [
+                        Text('Author : Muhammad Azwar'), // Menampilkan nama penulis berita
+                        Text('Sumber : detik.com'), // Menampilkan sumber berita
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
 }
